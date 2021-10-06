99eb30404d5ab0166fe83938778d7e7f62ed46da
vikas kumar <vk.vikas3003@gmail.com>
2021-10-06 16:57:08 +0530

    dynamics rendering added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import "./App.css";
 import Form from "./components/Form";
 import List from "./components/List";
+import { useState } from "react";
 
 let arr = [
   {
-    id: 1,
+    id: '1',
     title: "buy a shirt",
   },
   {
-    id: 2,
+    id: '2',
     title: "build a todolist app",
   },
   {
-    id: 3,
+    id: '3',
     title: "call someone",
   },
 ];
 
 function App() {
+  const [array, setArray] = useState(arr);
 
-  const addSaveHandler = (ourSubmittedData) => {
+  const addSaveHandler = (enteredtile) => {
     const newData = {
-      ...ourSubmittedData,
-      id: Math.random().toString()
-    }
+      id: Math.random().toString(),
+      title: enteredtile,
+    };
 
-    console.log(newData);
+    console.log([newData, ...array]);
+
+    setArray([newData, ...array]);
   };
 
   return (
     <div className="App">
       <Form onSave={addSaveHandler} />
-      <List dataArr={arr} />
+      <List dataArr={array} />
     </div>
   );
 }
